dcce256ec0eb99411b9349caaf6978b96c3fd01b
mattys1 <mattys0082@gmail.com>
2024-10-20 16:54:31 +0200

    Add button that opens the file picker

--- app/src/main/java/com/example/raptor/MainActivity.kt
+++ app/src/main/java/com/example/raptor/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.raptor
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.raptor.ui.theme.RaptorTheme
 
@@ -20,8 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             RaptorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    StartScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +39,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun StartScreen(modifier: Modifier = Modifier) {
+    val folderPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.OpenDocumentTree()
+    ) { uri: Uri? ->
+        uri?.let {
+            Log.d("FolderPicker", "Selected: $it")
+        }
+    }
+    Box(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .background(Color.Gray)
+    ) {
+        Button(
+            onClick = {
+                folderPickerLauncher.launch(null)
+            },
+            modifier = modifier
+                .align(Alignment.Center)
+        ) {
+            Text("Button")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun StartScreenPreview() {
     RaptorTheme {
-        Greeting("Android")
+        StartScreen()
     }
 }
