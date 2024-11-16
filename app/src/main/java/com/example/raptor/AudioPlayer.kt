ba0a97acb216b683fc6cb41b4aa8261833bd46bc
mattys1 <mattys0082@gmail.com>
2024-11-16 13:20:42 +0100

    AudioPlayer.kt class blueprint

---
+++ app/src/main/java/com/example/raptor/AudioPlayer.kt
@@ -0,0 +1,23 @@
+package com.example.raptor
+
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+import android.net.Uri
+
+class AudioPlayer(val context: Context) {
+    private val player = MediaPlayer().apply {
+        AudioAttributes.Builder()
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .build()
+    }
+
+    fun playUri(uri: Uri) {
+        player.apply {
+            setDataSource(context, uri)
+            prepare()
+            start()
+        }
+    }
+}
