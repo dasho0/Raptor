1ee5e43a86864fecb4890ce4d3a7f36c630ef086
mattys1 <mattys0082@gmail.com>
2024-12-01 18:03:08 +0100

    make the player text less wide

--- app/src/main/java/com/example/raptor/screens/SongPlayUI.kt
+++ app/src/main/java/com/example/raptor/screens/SongPlayUI.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Icon
@@ -27,6 +28,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
@@ -34,6 +36,7 @@ import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.room.util.TableInfo
 import com.example.raptor.viewmodels.PlayerViewModel
 import java.nio.file.WatchEvent
 
@@ -76,16 +79,22 @@ fun CurrentSongInfo(title: String?, artists: String?, cover: ImageBitmap, modifi
 
         Spacer(modifier = modifier.height(8.dp))
 
-        Text(
-            title?: "Unknown",
-            color = Color.White,
-            fontSize = 24.sp,
-        )
-        Text(
-            artists ?: "Unknown",
-            color = Color.White,
-            fontSize = 16.sp
-        )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier.width(300.dp)
+        ) {
+            Text(
+                title?: "Unknown",
+                color = Color.White,
+                fontSize = 24.sp,
+            )
+
+            Text(
+                artists ?: "Unknown",
+                color = Color.White,
+                fontSize = 16.sp,
+            )
+        }
     }
 }
 
