142a5875af6cbedc24f8434e4b4880ae2789d071
mattys1 <mattys0082@gmail.com>
2024-12-30 16:01:18 +0100

    ImageManager.kt

--- app/src/main/java/com/example/raptor/ImageManager.kt
+++ app/src/main/java/com/example/raptor/ImageManager.kt
@@ -1,7 +1,6 @@
 package com.example.raptor
 
 import android.content.Context
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.media.MediaMetadataRetriever
 import android.net.Uri
@@ -11,12 +10,28 @@ import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.util.fastJoinToString
 import androidx.core.net.toUri
 import dagger.hilt.android.qualifiers.ApplicationContext
+import org.jetbrains.annotations.NotNull
 import java.io.File
 import javax.inject.Inject
 
+/**
+ * Handles the extraction of album images from app storage and provides images from
+ * app storage to the rest of the program
+ */
 class ImageManager @Inject constructor(@ApplicationContext private val context: Context) {
     // TODO: Probably should check whether an image file doesn't exist and then write to it - but
     //  then what if an album image changes?
+
+    // TODO: should pass a MetadataRetriever as a param to not create a million of them
+    /**
+     * Extracts the image directly from some audio file and copies it into app storage
+     *
+     * @param uri Image URI
+     * @param artistNames List of names of the composers of the file
+     * @param albumName Name of the album of the file
+     *
+     * @return URI of the file created in app storage or `null`
+     */
     fun extractAlbumimage(
         uri: Uri?,
         artistNames: List<String>,
@@ -40,7 +55,13 @@ class ImageManager @Inject constructor(@ApplicationContext private val context: 
         return bitmapFile.toUri()
     }
 
-    fun collectBitmapFromUri(uri: Uri?): ImageBitmap {
+    /**
+     * Retrieves a image bitmap in local storage
+     *
+     * @param uri URI of the image file from local storage
+     * @return ImageBitmap from storage or blank bitmap
+     */
+    fun getBitmapFromAppStorage(uri: Uri?): ImageBitmap {
         Log.d(javaClass.simpleName, "Collecting bitmap with uri: $uri")
         if(uri != null) {
             context.contentResolver.openInputStream(Uri.parse(uri.toString())).use {
