305e124bb7bb90edc2ea4ba995ff6a6ce20c99fd
mattys1 <mattys0082@gmail.com>
2024-12-30 16:35:37 +0100

    Raptor.kt

--- app/src/main/java/com/example/raptor/Raptor.kt
+++ app/src/main/java/com/example/raptor/Raptor.kt
@@ -3,6 +3,11 @@ package com.example.raptor
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * Dummy class for dependency injection
+ *
+ * Can store application-wide data.
+ */
 @HiltAndroidApp
 class Raptor: Application() {
 }
